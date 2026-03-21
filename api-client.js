2c5c89610b3df0c9a7c5d93d5c0796096490e947
brisamenos <brisamenos@gmail.com>
2026-03-21 19:11:40 -0300

    Update api-client.js

--- api-client.js
+++ api-client.js
@@ -144,7 +144,7 @@
       };
 
       ['INSERT', 'UPDATE', 'DELETE'].forEach(evType => {
-        ['orders', 'mesas', 'menu_items', 'categories', 'store_config', 'garcons'].forEach(tbl => {
+        ['orders', 'mesas', 'menu_items', 'categories', 'store_config', 'garcons', 'customers'].forEach(tbl => {
           self._sse.addEventListener(`${tbl}:${evType}`, handle);
         });
       });
